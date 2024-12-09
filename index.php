4623d09210e00aa1d01283e559cf709d1fce640c
Rotdzaii <nguyen.manhhoangnguyenn@gmail.com>
2024-12-10 05:15:26 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+include "header.php";
+include "leftside.php";
+?>
+        <div class="admin-content-right">
+           <div class="admin-content-right-bg">
+               <img src="icon/giao diện.jpg" alt="">
+           </div>
+        </div>
+    </section>
+    <section>
+    </section>
+    <script src="js/script.js"></script>
+</body>
+</html>  
